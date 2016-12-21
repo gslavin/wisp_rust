4ecea57e856256b726c92dd04c2428b904fcc80f
George Slavin <george.r.slavin@gmail.com>
2016-12-20 21:30:46 -0500

    Cleaned up parser logic and added more tests

--- src/parser.rs
+++ src/parser.rs
@@ -22,18 +22,15 @@ pub fn parse<I>(tokens: &mut I) -> Node
     where I: Iterator<Item=Token>
 {
     let mut expr: Vec<Box<Node>> = Vec::new();
-    let mut end_expr = false;
     while let Some(token) = (*tokens).next() {
         match token {
             Token::OpenParen => expr.push(Box::new(parse(tokens))),
             Token::Number(x) => expr.push(Box::new(Node::Number(x))),
             Token::Identifier(x) => expr.push(Box::new(Node::Identifier(x))),
-            Token::CloseParen => end_expr = true,
-        }
-        if end_expr {
-            break;
+            Token::CloseParen => break,
         }
     }
+
     return Node::Expression(expr);
 }
 
@@ -71,4 +68,21 @@ mod test {
         assert_eq!(ast, expected_ast);
     }
 
+    #[test]
+    fn first_arg_nested_parse() {
+        let tokens = vec![Token::OpenParen, Token::Identifier(String::from("+")),
+                          Token::OpenParen, Token::Identifier(String::from("*")),
+                          Token::Number(3.0), Token::Number(4.0), Token::CloseParen,
+                          Token::Number(3.0), Token::CloseParen];
+        let expected_ast = Node::Expression(vec![Box::new(
+                           Node::Expression(vec![Box::new(Node::Identifier(String::from("+"))),
+                                                 Box::new(Node::Expression(vec![
+                                                    Box::new(Node::Identifier(String::from("*"))),
+                                                    Box::new(Node::Number(3.0)),
+                                                    Box::new(Node::Number(4.0))])),
+                                                    Box::new(Node::Number(3.0))]))]);
+        let ast = parse(&mut tokens.into_iter());
+        assert_eq!(ast, expected_ast);
+    }
+    // TODO: Add failure cases
 }
