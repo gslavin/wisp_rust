e82a909d9e34ab97d67f9cf7f9a563f09cba2e61
George Slavin <george.r.slavin@gmail.com>
2017-01-01 23:09:29 -0500

    Modified main function to parse and eval a series of expressions
    Instead of just the a single expression.  All expressions are evaluated
    in to the same context

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,17 @@ fn read_file(filename: &str) -> Result<String, Error> {
 }
 
 fn main() {
-
 	let result = read_file("input.wsp");
 	let s = result.expect("Unable to read file");
 	println!("{}", s);
 	let tokens = lexer::parse(s.as_str());
-    let mut ast = parser::parse(&mut tokens.into_iter().peekable());
+    let mut token_iter = tokens.into_iter().peekable();
+
+    // Parse and evaluate each expression until end of file
     let mut c = eval::Context::new();
-    eval::eval(&mut ast, &mut c);
-    println!("{:?}", ast);
+    while let Some(_) = token_iter.peek() {
+        let mut ast = parser::parse(&mut token_iter);
+        eval::eval(&mut ast, &mut c);
+        println!("{:?}", ast);
+    }
 }
